f53e8352521a747bb6d789f183d21f9a2a1fe3c1
green-study <soo010410@naver.com>
2026-04-02 19:54:00 +0900

    default test code

--- tests/isAdult.test.js
+++ tests/isAdult.test.js
@@ -1,9 +1,11 @@
 const isAdult = require("../src/isAdult");
 
-test("20살 이상이면 성인이다", () => {
-  expect(isAdult(20)).toBe(true);
-});
+describe("isAdult 함수 테스트", () => {
+  test("20살 이상이면 true를 반환한다", () => {
+    expect(isAdult(20)).toBe(true);
+  });
 
-test("19살은 미성년자이다", () => {
-  expect(isAdult(19)).toBe(false);
+  test("19살이면 false를 반환한다", () => {
+    expect(isAdult(19)).toBe(false);
+  });
 });
