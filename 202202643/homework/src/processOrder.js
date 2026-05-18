3937548eaa6f08e26dceb602e31654898fff4f36
chominjeong <chomj4836@gmail.com>
2026-05-18 16:36:04 +0900

    practice, homework 코드 제출

---
+++ 202202643/homework/src/processOrder.js
@@ -0,0 +1,15 @@
+function processOrder(api, order) {
+    if (!order || typeof order.price !== "number") {
+        throw new Error("invalid order");
+    }
+
+    if (order.price >= 50000) {
+        api.sendDiscountNotification(order.userId);
+    } else {
+        api.sendBasicNotification(order.userId);
+    }
+
+    return order.price;
+}
+
+module.exports = processOrder;
