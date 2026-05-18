a29cc271697623ded399d1170851628253ab1499
EomTeaEun <xodms3320@gmail.com>
2026-05-18 17:43:53 +0900

    실습, 과제 코드

---
+++ 202402697/practice/tests/add.test.js
@@ -0,0 +1,13 @@
+const add = require("../src/add");
+
+test("두 수 더하기", () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test("문자열 숫자도 더하기", () => {
+  expect(add("2", "3")).toBe(5);
+});
+
+test("잘못된 입력 시 에러 발생", () => {
+  expect(() => add("abc", 3)).toThrow();
+});
