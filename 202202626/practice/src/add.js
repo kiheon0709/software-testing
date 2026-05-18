02d574059db71080332d64841cb2280e4e6a0878
T1M1T1 <timiti0206@naver.com>
2026-05-18 16:05:47 +0900

    feat: 과제 코드 제출

---
+++ 202202626/practice/src/add.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+  const numA = Number(a);
+  const numB = Number(b);
+  if (isNaN(numA) || isNaN(numB)) {
+    throw new Error("invalid input");
+  }
+  return numA + numB;
+}
+
+module.exports = add;
