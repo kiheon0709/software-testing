53a0b62628fa564f5b1ee9f3cd44e6863d6314f4
성채원 <julia2003a@naver.com>
2026-05-18 16:18:32 +0900

    [과제] 202302565_성채원

---
+++ 202302565/homework/tests/processOrder.test.js
@@ -0,0 +1,52 @@
+const processOrder = require("../src/processOrder");
+
+describe("processOrder", () => {
+  
+  test("order가 없으면 에러 발생", () => {
+    const mockApi = {};
+    expect(() => {
+      processOrder(mockApi, null);
+    }).toThrow("invalid order");
+  });
+
+  test("price가 숫자가 아니면 에러 발생", () => {
+    const mockApi = {};
+    
+    expect(() => {
+      processOrder(mockApi, { price: "50000", userId: 1 }); // 숫자가 아닌 문자열 주입
+    }).toThrow("invalid order");
+  }); 
+
+  test("price >= 50000이면 할인 알림 호출", () => {
+    const mockApi = {
+      sendDiscountNotification: jest.fn(),
+      sendBasicNotification: jest.fn(),
+    };
+
+    processOrder(mockApi, { price: 60000, userId: 1 });
+    expect(mockApi.sendDiscountNotification).toHaveBeenCalledTimes(1);
+    expect(mockApi.sendDiscountNotification).toHaveBeenCalledWith(1);
+    expect(mockApi.sendBasicNotification).not.toHaveBeenCalled();
+  }); 
+
+  test("price < 50000이면 기본 알림 호출", () => {
+    const mockApi = {
+      sendDiscountNotification: jest.fn(),
+      sendBasicNotification: jest.fn(),
+    };
+
+    processOrder(mockApi, { price: 10000, userId: 2 });
+
+    expect(mockApi.sendBasicNotification).toHaveBeenCalledTimes(1);
+    expect(mockApi.sendBasicNotification).toHaveBeenCalledWith(2);
+    expect(mockApi.sendDiscountNotification).not.toHaveBeenCalled();
+  }); 
+
+  test("price를 반환한다", () => {
+    const mockApi = {
+      sendBasicNotification: jest.fn(),
+    };
+    const result = processOrder(mockApi, { price: 30000, userId: 1 });
+    expect(result).toBe(30000);
+  }); 
+});
