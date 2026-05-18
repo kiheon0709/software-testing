79473ff69a0b0cf9a0795c5d979dbe3de46b25ec
leesubeen <ahdtnsdl1914@gmail.com>
2026-05-18 16:35:13 +0900

    processOrder.test.js 테스트케이스 수정

--- 202402716/homework/tests/processOrder.test.js
+++ 202402716/homework/tests/processOrder.test.js
@@ -1,13 +1,5 @@
 const processOrder = require("../src/processOrder"); 
 
-describe("processOrder", () => {
-  test("order가 없으면 에러 발생", () => {});
-  test("price가 숫자가 아니면 에러 발생", () => {});
-  test("price >= 50000이면 할인 알림 호출", () => {});
-  test("price < 50000이면 기본 알림 호출", () => {});
-  test("price를 반환한다", () => {});
-});
-
 test("order가 없으면 에러 발생", () => {
     const mockApi = {};
     expect(() => processOrder(mockApi, null)).toThrow();
